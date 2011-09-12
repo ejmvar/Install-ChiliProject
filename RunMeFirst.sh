362d3991c91ca2732fe6e72a446eabf0f3a141b5
Mark Frandson <mark80537@gmail.com>
2011-09-12 09:32:25 -0600

    Edited RunMeFirst.sh via GitHub

--- RunMeFirst.sh
+++ RunMeFirst.sh
@@ -3,18 +3,18 @@
 #####
 # Prerequisite script for chiliproject installation on Ubuntu 10.04 LTS
 #####
-# This script will install expect and mysql-server and phpmyadmin applications and then
+# This script will install expect, autoexpect (expect-dev) mysql-server and phpmyadmin applications and then
 # call chiliproject.exp to finish installing chiliproject on Ubuntu 10.04 LTS
 #####
 
-# check for being root
+# exit if not root
 if [[ ${UID} != 0 ]]; then
   echo "You must be a root user" 2>&1
   exit 1
 fi
 
-# install expect and nullmailer package
+# install expect, autoexpect, mysql and phpmyadmin packages
 apt-get -qqy install expect mysql-server phpmyadmin
 
-# launch expect script to set up ledger smb
+# launch expect script to set up ChiliProject
 ./chiliproject.exp
